a534920ba7c991ae89c70b104e8461dbb620f7bc
Sakshi Bhalerao <sakshibhalerao58@gmail.com>
2026-04-26 14:33:33 +0530

    adding server js

--- server.js
+++ server.js
@@ -1,40 +1,21 @@
-require("dotenv").config();
-
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5500",
-    "https://vercel-frontend-gi2k.vercel.app/"
-  ]
-}));
+app.use(cors());
 app.use(express.json());
-
-// Root
-app.get("/", (req, res) => {
-  res.send("📚 Library API Running 🚀");
-});
-
-// DB connect
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => {
-    console.error("❌ DB ERROR:", err);
-  });
 
 // Routes
 app.use("/api/books", require("./routes/bookRoutes"));
 
-// Error Handler (IMPORTANT)
-app.use((err, req, res, next) => {
-  console.error("🔥 ERROR:", err.message);
-  res.status(500).json({ error: err.message });
-});
-
-// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
